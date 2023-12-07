ed8d1d050e73cd278d5c2b7bf3f188e18ef1f6e9
Nemanja Vlahovic <nemanja@nemanjavlahovic.com>
2023-12-07 02:27:57 +0100

    Initial commit (add CryptoTracker app)

---
+++ CryptoTracker/CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Nemanja Vlahovic on 7.12.23..
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
